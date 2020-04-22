501beedce902f1f6271126fb11749ea4f60b1260
cs329yangzhong <yang.zhong@utexas.edu>
2020-04-22 18:50:46 -0400

    add bias task

--- experiments/scripts/run_bias_detection.sh
+++ experiments/scripts/run_bias_detection.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python ner.py --config configs/ner/conll03.json --num_epochs 400 --batch_size 16 \
  --loss_type sentence --optim sgd --learning_rate 0.01 --lr_decay 0.99999 --grad_clip 0.0 --warmup_steps 100 --weight_decay 0.0 --unk_replace 0.0 \
- --embedding glove --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz" --model_path "models/ner/conll03" \
- --train "data/conll2003/eng.train.bio.conll" --dev "data/conll2003/eng.dev.bio.conll" --test "data/conll2003/eng.test.bio.conll"
+ --embedding glove --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz" --model_path "models/bias_tagging/" \
+ --train "data/bias_detection/eng.train.bio.conll" --dev "data/bias_detection/eng.dev.bio.conll" --test "data/bias_detection/eng.test.bio.conll"
